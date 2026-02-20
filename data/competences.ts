532a87cde7be9fec2881db6e3d7488d1549dbc6c
basjardin robin <basjardinrobin@gmail.com>
2026-02-20 19:11:37 +0100

    fix: prefixer chemins images avec basePath pour GitHub Pages

--- data/competences.ts
+++ data/competences.ts
@@ -4,83 +4,83 @@ export interface Competence {
   description: string;
 }
 
+const base = process.env.NEXT_PUBLIC_BASE_PATH ?? '';
+
 export const competenceData: Competence[] = [
   {
     nom: "HTML",
-    path: "/icons/html.svg",
+    path: `${base}/icons/html.svg`,
     description:
       "Permet de structurer le texte, les images et les liens d'une page.",
   },
   {
     nom: "JS",
-    path: "/icons/js.svg",
+    path: `${base}/icons/js.svg`,
     description:
       "Ajoute de l'interaction (boutons, calculs, animations) aux sites.",
   },
   {
     nom: "TypeScript",
-    path: "/icons/typescript.svg",
+    path: `${base}/icons/typescript.svg`,
     description:
       "Ajoute du typage à JavaScript pour éviter les erreurs.",
   },
   {
     nom: "CSS",
-    path: "/icons/css.svg",
+    path: `${base}/icons/css.svg`,
     description: "Sert à définir les couleurs, les polices et la mise en page.",
   },
   {
     nom: "PHP",
-    path: "/icons/php.svg",
+    path: `${base}/icons/php.svg`,
     description:
       "Gère les comptes utilisateurs, les formulaires et les bases de données.",
   },
   {
     nom: "SQL",
-    path: "/icons/sql.svg",
+    path: `${base}/icons/sql.svg`,
     description: "Langage utilisé pour communiquer avec les bases de données.",
   },
   {
     nom: "Bootstrap",
-    path: "/icons/Bootstrap.svg",
+    path: `${base}/icons/Bootstrap.svg`,
     description:
       "Outil pour créer rapidement des sites qui s'adaptent aux mobiles.",
   },
   {
     nom: "tailwind",
-    path: "/icons/tailwind.svg",
+    path: `${base}/icons/tailwind.svg`,
     description:
       "Permet de créer des designs modernes et précis très rapidement.",
   },
   {
     nom: "Git",
-    path: "/icons/git.svg",
+    path: `${base}/icons/git.svg`,
     description:
       "Logiciel qui enregistre l'historique du projet pour éviter les erreurs.",
   },
   {
     nom: "Node",
-    path: "/icons/node.svg",
+    path: `${base}/icons/node.svg`,
     description:
       "Permet d'utiliser JavaScript pour faire fonctionner tout le côté serveur.",
   },
   {
     nom: "React",
-    path: "/icons/react.svg",
+    path: `${base}/icons/react.svg`,
     description:
       "Outil pour construire des interfaces fluides et rapides pour l'utilisateur.",
   },
   {
     nom: "Next",
-    path: "/icons/next.svg",
+    path: `${base}/icons/next.svg`,
     description:
       "Outil pour construire des sites web rapides et fluides.",
   },
   {
     nom: "Wordpress",
-    path: "/icons/wordpress.svg",
+    path: `${base}/icons/wordpress.svg`,
     description:
       "Outil pour construire des sites web facilement et rapidement.",
   },
 ];
-
-
--- data/projects.ts
+++ data/projects.ts
@@ -9,11 +9,13 @@ export interface ProjectItem {
   badges: BadgeProps[];
 }
 
+const base = process.env.NEXT_PUBLIC_BASE_PATH ?? '';
+
 export const projectsData: ProjectItem[] = [
   {
     targetCarouselId: "carousel-1",
     title: "Premier site web",
-    image: "/img/premier-site-web.png",
+    image: `${base}/img/premier-site-web.png`,
     description:
       "Mon premier site web, réalisé avec HTML, CSS et JavaScript.",
     link: "https://github.com/basjardin/premier_site_web",
@@ -26,7 +28,7 @@ export const projectsData: ProjectItem[] = [
   {
     targetCarouselId: "carousel-1",
     title: "Premier Portfolio",
-    image: "/img/premier portfolio.png",
+    image: `${base}/img/premier portfolio.png`,
     description:
       "Mon premier portfolio réalisé avec HTML, CSS et JavaScript.",
     link: "https://basjardin.github.io/porfolio/",
@@ -39,7 +41,7 @@ export const projectsData: ProjectItem[] = [
   {
     targetCarouselId: "carousel-1",
     title: "Application pour prof bénévole",
-    image: "/img/pas image.png",
+    image: `${base}/img/pas image.png`,
     description:
       "Application en PHP pour les professeurs bénévoles pour aider les élèves en difficulté scolaire.",
     link: "https://github.com/basjardin/SAE401",
@@ -52,7 +54,7 @@ export const projectsData: ProjectItem[] = [
   {
     targetCarouselId: "carousel-2",
     title: "jeux vidéo unity",
-    image: "/img/pas image.png",
+    image: `${base}/img/pas image.png`,
     description: "premeir jeux vidéo 3D créer sur unity.",
     link: "https://github.com/basjardin/Anomalia",
     badges: [
@@ -63,7 +65,7 @@ export const projectsData: ProjectItem[] = [
   {
     targetCarouselId: "carousel-1",
     title: "App en codeigniter",
-    image: "/img/pas image.png",
+    image: `${base}/img/pas image.png`,
     description: "premiere app créer avec codeigniter",
     link: "https://github.com/basjardin/reservation",
     badges: [
@@ -75,7 +77,7 @@ export const projectsData: ProjectItem[] = [
   {
     targetCarouselId: "carousel-2",
     title: "Autre projet Unity",
-    image: "/img/pas image.png",
+    image: `${base}/img/pas image.png`,
     description: "Un autre projet de jeux vidéo réalisé avec Unity.",
     link: "https://github.com/basjardin/R307-shooter",
     badges: [
@@ -86,7 +88,7 @@ export const projectsData: ProjectItem[] = [
   {
     targetCarouselId: "carousel-1",
     title: "curseforge mod award 2025",
-    image: "/img/pas image.png",
+    image: `${base}/img/pas image.png`,
     description:
       "site pour voir les mods qu'ont pouvez voter pendant le mod award 2025.",
     link: "https://basjardin.github.io/curseforge-mod-award-2025/",
